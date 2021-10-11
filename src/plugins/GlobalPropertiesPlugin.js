260826d129edbb908af3641de2c4207f7d3077db
Luka Bagic <luka.bagic@sintezis.co>
2021-10-12 00:30:38 +0200

    Add capitalize util

--- src/plugins/GlobalPropertiesPlugin.js
+++ src/plugins/GlobalPropertiesPlugin.js
@@ -1,4 +1,5 @@
 import { routes } from "../router/routes";
+import { capitalize } from "../utils/capitalize";
 import { cssVariables } from "../utils/cssVariables";
 
 export default {
@@ -6,6 +7,7 @@ export default {
     Object.entries({
       $cssVariables: cssVariables,
       $routes: routes,
+      $capitalize: capitalize,
     }).forEach(([key, value]) => (app.config.globalProperties[key] = value));
   },
 };

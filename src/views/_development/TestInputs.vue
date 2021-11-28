c7e6f258595a0a4fb82bf3240501aabb86525db9
Luka Bagic <luka.bagic@sintezis.co>
2021-11-28 20:07:35 +0100

    Update test page

--- src/views/_development/TestInputs.vue
+++ src/views/_development/TestInputs.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
+    <p>Basic inputs</p>
     <div class="test-wrapper">
-      <snt-input v-model="form.text" type="text" label="Text" required />
+      <snt-input v-model="basic.text" type="text" label="Text" required />
+      <snt-input v-model="basic.email" type="email" label="Email" required />
       <snt-input
-        v-model="form.textOptions"
+        v-model="basic.password"
+        type="password"
+        label="Password"
+        required
+      />
+      <snt-input v-model="basic.search" type="search" label="Search" required />
+      <snt-input v-model="basic.number" type="number" label="Number" required />
+      <snt-input v-model="basic.tel" type="tel" label="Telephone" required />
+      <snt-input v-model="basic.url" type="url" label="Url" required />
+      <snt-input
+        v-model="basic.textarea"
+        type="textarea"
+        label="Textarea"
+        required
+      />
+    </div>
+    <button @click="basic.text = basic.text === 'asdf' ? 'qwe' : 'asdf'">
+      change model
+    </button>
+    <pre>{{ basic }}</pre>
+    <p>Other inputs</p>
+    <div class="test-wrapper">
+      <snt-input
+        v-model="other.textOptions"
         type="text"
         label="Text (options)"
         :options="[
@@ -12,50 +37,33 @@
         ]"
         required
       />
-      <snt-input v-model="form.email" type="email" label="Email" required />
+      <snt-input v-model="other.date" type="date" label="Date" required />
       <snt-input
-        v-model="form.password"
-        type="password"
-        label="Password"
-        required
-      />
-      <snt-input v-model="form.date" type="date" label="Date" required />
-      <snt-input
-        v-model="form.dateRange"
+        v-model="other.dateRange"
         type="date"
         label="Date (range)"
         required
         :datetimeOptions="{ range: true }"
       />
-      <snt-input v-model="form.time" type="time" label="Time" required />
-      <snt-input v-model="form.month" type="month" label="Month" required />
-      <snt-input v-model="form.search" type="search" label="Search" required />
-      <snt-input v-model="form.number" type="number" label="Number" required />
-      <snt-input v-model="form.tel" type="tel" label="Telephone" required />
-      <snt-input v-model="form.color" type="color" label="Color" required />
-      <snt-input v-model="form.url" type="url" label="Url" required />
+      <snt-input v-model="other.time" type="time" label="Time" required />
+      <snt-input v-model="other.month" type="month" label="Month" required />
+      <snt-input v-model="other.color" type="color" label="Color" required />
       <snt-input
-        v-model="form.checkbox"
+        v-model="other.checkbox"
         type="checkbox"
         label="Checkbox"
         required
       />
       <snt-input
-        v-model="form.radio"
+        v-model="other.radio"
         type="radio"
         label="Im radio label"
         required
       />
-      <snt-input v-model="form.range" type="range" label="Range" required />
-      <snt-input v-model="form.file" type="file" label="File" required />
+      <snt-input v-model="other.range" type="range" label="Range" required />
+      <snt-input v-model="other.file" type="file" label="File" required />
       <snt-input
-        v-model="form.textarea"
-        type="textarea"
-        label="Textarea"
-        required
-      />
-      <snt-input
-        v-model="form.select"
+        v-model="other.select"
         type="select"
         label="Select"
         required
@@ -69,7 +77,7 @@
         ]"
       />
     </div>
-    <pre>{{ form }}</pre>
+    <pre>{{ other }}</pre>
   </div>
 </template>
 
@@ -81,26 +89,28 @@ export default {
   name: "TestInputs",
   data() {
     return {
-      form: {
-        text: "",
-        textOptions: "",
+      basic: {
+        text: "asdf",
         email: "",
         password: "",
-        date: "",
-        dateRange: "",
-        time: "",
-        month: "",
         search: "",
         number: "",
         tel: "",
-        color: "",
         url: "",
-        checkbox: "",
+        textarea: "",
+      },
+      other: {
+        textOptions: "",
+        date: "2021-11-04T19:01:00.000Z",
+        dateRange: "",
+        time: "",
+        month: "",
+        color: "",
+        checkbox: false,
         radio: "",
         range: "",
         file: "",
-        textarea: "",
-        select: "",
+        select: 2,
       },
     };
   },

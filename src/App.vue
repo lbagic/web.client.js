c808fa68f5ffba00abddedb0a7d83619fab37c53
Luka Bagic <luka.bagic@sintezis.co>
2021-11-29 23:56:25 +0100

    Add toasts to app by default

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <navigation />
   <router-view />
   <component :is="developmentPopup" />
+  <snt-toast />
 </template>
 
 <script>
 import { defineAsyncComponent } from "@vue/runtime-core";
 import Navigation from "./components/Navigation.vue";
+import SntToast from "./components/utils/SntToast.vue";
 
 export default {
   name: "App",
-  components: { Navigation },
+  components: { Navigation, SntToast },
   computed: {
     developmentPopup() {
       return process.env.NODE_ENV === "development"
@@ -27,7 +29,7 @@ export default {
 #app {
   min-height: 100%;
   display: grid;
-  // align-content: start;
+  align-content: start;
   grid-template-rows: auto 1fr;
   background: var(--snt-app-background);
   color: var(--snt-app-color);

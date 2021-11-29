20f713fff661e5d307138374804616336b42a0de
Luka Bagic <luka.bagic@sintezis.co>
2021-11-29 23:59:07 +0100

    Updates

--- src/views/_development/Test.vue
+++ src/views/_development/Test.vue
@@ -2,24 +2,12 @@
   <div class="wrapper">
     <router-link to="/development/documentation">Documentation</router-link>
     <router-link to="/development/inputs">Test inputs</router-link>
-    <datepicker ref="datepicker" v-model="qwe" />
-    <pre>{{ qwe }}</pre>
   </div>
 </template>
 
 <script>
-import "vue3-date-time-picker/dist/main.css";
-import Datepicker from "vue3-date-time-picker";
-
 export default {
   name: "Test",
-  components: { Datepicker },
-  mounted() {},
-  data() {
-    return {
-      qwe: new Date(),
-    };
-  },
 };
 </script>
 

dfdf8dca07bdc58f748a85d3980504cc18e8bce8
Luka Bagic <luka.bagic@sintezis.co>
2021-12-15 09:32:34 +0100

    Setting up docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,67 @@
 module.exports = {
-  title: "Hello VitePress",
+  title: "web.stack.js",
   description: "Just playing around.",
+  nav: [
+    { text: "Guide", link: "/", activeMatch: "^/$|^/guide/" },
+    {
+      text: "Config Reference",
+      link: "/config/basics",
+      activeMatch: "^/config/",
+    },
+    {
+      text: "Release Notes",
+      link: "https://github.com/vuejs/vitepress/releases",
+    },
+  ],
+  sidebar: {
+    "/guide/": getGuideSidebar(),
+    "/config/": getConfigSidebar(),
+    "/": getGuideSidebar(),
+  },
 };
+
+function getGuideSidebar() {
+  return [
+    {
+      text: "Introduction",
+      children: [
+        { text: "What is VitePress?", link: "/" },
+        { text: "Getting Started", link: "/guide/getting-started" },
+        { text: "Configuration", link: "/guide/configuration" },
+        { text: "Asset Handling", link: "/guide/assets" },
+        { text: "Markdown Extensions", link: "/guide/markdown" },
+        { text: "Using Vue in Markdown", link: "/guide/using-vue" },
+        { text: "Deploying", link: "/guide/deploy" },
+      ],
+    },
+    {
+      text: "Advanced",
+      children: [
+        { text: "Frontmatter", link: "/guide/frontmatter" },
+        { text: "Theming", link: "/guide/theming" },
+        { text: "API Reference", link: "/guide/api" },
+        {
+          text: "Differences from Vuepress",
+          link: "/guide/differences-from-vuepress",
+        },
+      ],
+    },
+  ];
+}
+
+function getConfigSidebar() {
+  return [
+    {
+      text: "App Config",
+      children: [{ text: "Basics", link: "/config/basics" }],
+    },
+    {
+      text: "Theme Config",
+      children: [
+        { text: "Homepage", link: "/config/homepage" },
+        { text: "Algolia Search", link: "/config/algolia-search" },
+        { text: "Carbon Ads", link: "/config/carbon-ads" },
+      ],
+    },
+  ];
+}
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,29 @@
-# Hello VitePress
+<!-- ---
+home: true
+heroImage: /logo.png
+heroAlt: Logo image
+heroText: Hero Title
+tagline: Hero subtitle
+actionText: Get Started
+actionLink: /guide/
+features:
+  - title: Simplicity First
+    details: Minimal setup with markdown-centered project structure helps you focus on writing.
+  - title: Vue-Powered
+    details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+  - title: Performant
+    details: VitePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+footer: MIT Licensed | Copyright © 2019-present Evan You
+--- -->
+
+# What is web.stack.js
+
+A frontend framework solution built on top of vue.js
+
+## i am a h2
+
+### i am a h3
+
+The thing is
+
+Need to [contact someone](./components/index.md) on the team?

4e2e23a0d758d89b9edab5a29925301ef4a5792d
Luka Bagic <luka.bagic@sintezis.co>
2021-12-19 08:49:51 +0100

    Update examples

--- src/components/examples/ExampleInput.vue
+++ src/components/examples/ExampleInput.vue
@@ -1,21 +1,21 @@
 <template>
-  <snt-input required label="text" type="text" />
-  <snt-input required label="email" type="email" />
-  <snt-input required label="password" type="password" />
-  <snt-input required label="search" type="search" />
-  <snt-input required label="tel" type="tel" />
-  <snt-input required label="url" type="url" />
-  <snt-input required label="color" type="color" />
-  <snt-input required label="number" type="number" />
-  <snt-input required label="range" type="range" />
-  <snt-input required label="checkbox" type="checkbox" />
-  <snt-input required label="radio" type="radio" />
-  <snt-input required label="file" type="file" />
-  <snt-input required label="textarea" type="textarea" />
-  <snt-input required label="select" type="select" />
-  <snt-input required label="date" type="date" />
-  <snt-input required label="time" type="time" />
-  <snt-input required label="month" type="month" />
+  <snt-input required label="type=text" type="text" />
+  <snt-input required label="type=email" type="email" />
+  <snt-input required label="type=password" type="password" />
+  <snt-input required label="type=search" type="search" />
+  <snt-input required label="type=tel" type="tel" />
+  <snt-input required label="type=url" type="url" />
+  <snt-input required label="type=color" type="color" />
+  <snt-input required label="type=number" type="number" />
+  <snt-input required label="type=range" type="range" />
+  <snt-input required label="type=checkbox" type="checkbox" />
+  <snt-input required label="type=radio" type="radio" />
+  <snt-input required label="type=file" type="file" />
+  <snt-input required label="type=textarea" type="textarea" />
+  <snt-input required label="type=select" type="select" />
+  <snt-input required label="type=date" type="date" />
+  <snt-input required label="type=time" type="time" />
+  <snt-input required label="type=month" type="month" />
 </template>
 
 <script>
